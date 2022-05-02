6f257de5caf91916eebf02e0a7ea504ef889b70a
Carolina Marin Hincapie - Ceiba Software <carolina.marin@ceiba.com.co>
2022-05-01 23:03:11 -0500

    Error jenkins

--- microservicio/src/main/java/com/ceiba/Application.java
+++ microservicio/src/main/java/com/ceiba/Application.java
@@ -1,12 +1,14 @@
 package com.ceiba;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({"com.ceiba"})
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
